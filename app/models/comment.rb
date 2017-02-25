0b8a38d354401565018ec5097b9748cd07eae00c
Miwa <a.cokegasuki@gmail.com>
2017-02-25 09:52:30 +0000

    validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :thre
+    
+    validates :thre, presence: true
+    validates :name, presence: true
+    validates :content, presence: true, length: { in: 1..1000 }
 end
